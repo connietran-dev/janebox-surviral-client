9be0542cd8a9a9b003ad1677120d8caf68a9b695
srsexton94 <srsexton94@gmail.com>
2020-07-10 23:04:24 -0400

    Add base structure to Home Page
    Add 'New Game' and 'Join Game' buttons to home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <button>New Game</button>
+      <form>
+        <input type="text" />
+        <button>Join Game</button>
+      </form>
+    </>
+  )
+}
