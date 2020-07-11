33c21ad794b429b1ff72e2e5a73011cda4e6eab5
srsexton94 <srsexton94@gmail.com>
2020-07-11 13:10:56 -0400

    Updates

---
+++ precache-manifest.418191bc1507fe991fae58d3b721a7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0f74ab83b6c1aeb79d8449db7c8bba",
+    "url": "/janebox-surviral-client/index.html"
+  },
+  {
+    "revision": "8879845d1a52a4f37f6c",
+    "url": "/janebox-surviral-client/static/css/main.1758dff8.chunk.css"
+  },
+  {
+    "revision": "e22585899f533b7a35cd",
+    "url": "/janebox-surviral-client/static/js/2.5348a295.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/janebox-surviral-client/static/js/2.5348a295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8879845d1a52a4f37f6c",
+    "url": "/janebox-surviral-client/static/js/main.b0ffa0bf.chunk.js"
+  },
+  {
+    "revision": "31aa2793a4b01b695a5e",
+    "url": "/janebox-surviral-client/static/js/runtime-main.deea5962.js"
+  }
+]);
