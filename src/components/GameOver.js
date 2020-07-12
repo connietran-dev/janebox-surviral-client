6381e809ea5826c6075950096af942cbabdf9122
srsexton94 <srsexton94@gmail.com>
2020-07-12 15:41:21 -0400

    Style GameOver component w/temporary result function

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -2,9 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function GameOver() {
+  const msg = result => {
+    if (result === 'win') {
+      return 'Congrats! You won!'
+    } else {
+      return "Damn... You lost :'("
+    }
+  }
   return (
     <section className="Game-Over">
       <h1>G a m e - O v e r</h1>
+      <p className="end-msg">{msg('lose')}</p>
       <Link to="/">
         <button className="admin-button">Play again!</button>
       </Link>
