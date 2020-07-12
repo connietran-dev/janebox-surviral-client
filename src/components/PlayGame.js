5f5f579ed411dfc6259f930b6fae89e276fc8537
srsexton94 <srsexton94@gmail.com>
2020-07-12 10:53:20 -0400

    Style team-log and game-content elements

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -8,13 +8,17 @@ export default function PlayGame() {
         <button className="admin-button">X</button>
       </Link>
       <main>
-      <section className="team-log"></section>
+      <section className="team-log">
+        <h5>Team Log</h5>
+        <p>+</p>
+      </section>
       <section className="game-content"></section>
       <section className="player-profile">
         <img src="" alt="" />
         <section className="char-descrip">
           <h3>The Engineer</h3>
-          <p>You’ll be responsible for rebuilding a whole new world… Sounds like a lot of work.</p>
+          <p>You’ll be responsible for rebuilding a whole new world…
+          Sounds like a lot of work.</p>
         </section>
       </section>
       </main>
