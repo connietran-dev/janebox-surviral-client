550b8c18e68a11232c6c353aba2876007de03029
srsexton94 <srsexton94@gmail.com>
2020-07-12 00:01:12 -0400

    Add and style main backdrop

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function PlayGame() {
   return (
-    <>
-      <h1>Play Game Page</h1>
+    <section className="Play-Game">
       <Link to="/game-over">
-        <button className="admin-button">All done!</button>
+        <button className="admin-button">X</button>
       </Link>
-    </>
+      <main>
+      </main>
+    </section>
   )
 }
