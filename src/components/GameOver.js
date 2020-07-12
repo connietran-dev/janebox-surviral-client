3ec9d3217b6464355df9e0dbb104aeacb0e5b9bc
srsexton94 <srsexton94@gmail.com>
2020-07-12 14:14:58 -0400

    Add imagery to PlayGame component

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function GameOver() {
   return (
-    <>
-      <h1>Game Over Page</h1>
+    <section className="Game-Over">
+      <h1>G a m e - O v e r</h1>
       <Link to="/">
         <button className="admin-button">Play again!</button>
       </Link>
-    </>
+    </section>
   )
 }
