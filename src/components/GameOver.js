e611944ef0b931dabf61d3a09553810d3ea0bdf8
srsexton94 <srsexton94@gmail.com>
2020-07-13 11:17:11 -0400

    Keeps track of individuals points to determine end

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function GameOver() {
-  const msg = result => {
-    if (result === 'win') {
+export default function GameOver({ location }) {
+  const points = parseInt(location.search.substring(18))
+
+  const msg = points => {
+    if (points > 6) {
       return 'Congrats! You won!'
     } else {
-      return "Damn... You lost :'("
+      return "Dang... You lost :'("
     }
   }
+  
   return (
     <section className="Game-Over">
       <h1>G a m e - O v e r</h1>
-      <p className="end-msg">{msg('lose')}</p>
+      <p className="end-msg">{msg(points)}</p>\
       <Link to="/">
         <button className="admin-button">Play again!</button>
       </Link>
