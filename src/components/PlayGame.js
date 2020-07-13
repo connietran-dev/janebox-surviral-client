03ee65473137cbe8fa7562673765ec3bf4b94629
srsexton94 <srsexton94@gmail.com>
2020-07-13 00:09:36 -0400

    Refactor PlayerProfile into own component

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import TeamLog from './TeamLog'
+import PlayerProfile from './PlayerProfile'
 
 export default function PlayGame() {
-
-  // function toggleTeamLog() {
-  //   var x = document.getElementById('players-ul')
-  //   if (x.style.display === "none") {
-  //     x.style.display = "inline-block";
-  //   } else {
-  //     x.style.display = "none";
-  //   }
-  // }
-
   return (
     <section className="Play-Game">
       <Link to="/game-over">
@@ -21,14 +12,7 @@ export default function PlayGame() {
       <main>
       <TeamLog />
       <section className="game-content"></section>
-      <section className="player-profile">
-        <img src="" alt="" />
-        <section className="char-descrip">
-          <h3>The Engineer</h3>
-          <p>You’ll be responsible for rebuilding a whole new world…
-          Sounds like a lot of work.</p>
-        </section>
-      </section>
+      <PlayerProfile />
       </main>
     </section>
   )
---
+++ src/components/PlayerProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PlayerProfile = () => {
+  return (
+    <section className="player-profile">
+      <img src="" alt="" />
+      <section className="char-descrip">
+        <h3>The Engineer</h3>
+        <p>You’ll be responsible for rebuilding a whole new world…
+        Sounds like a lot of work.</p>
+      </section>
+    </section>
+  )
+}
+
+export default PlayerProfile;
