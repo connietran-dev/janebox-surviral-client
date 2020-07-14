50f391eaf2f0ff49b48685777cee8822819a748e
srsexton94 <srsexton94@gmail.com>
2020-07-14 10:35:20 -0400

    Fix 'no noreferrer noopener' error with <a> tags
    Put the 'rel' property with 'noreferrer noopener' value at the start of
    a tag properties (doesn't work if it comes after the 'target' prop)

--- src/components/About.js
+++ src/components/About.js
@@ -13,25 +13,25 @@ function Home() {
       <div>
         <ul><h4>Code Source:</h4>
           <li>
-            <a href="https://github.com/connietran-dev/janebox-surviral-client" target="_blank" rel="noopener noreferer">Client Repo</a>
+            <a rel="noreferrer noopener" href="https://github.com/connietran-dev/janebox-surviral-client" target="_blank">Client Repo</a>
           </li>
           <li>
-            <a href="https://github.com/connietran-dev/janebox-surviral-server" target="_blank" rel="noopener noreferer">Server Repo</a>
+            <a rel="noreferrer noopener"href="https://github.com/connietran-dev/janebox-surviral-server" target="_blank">Server Repo</a>
           </li>
         </ul>
         <ul><h4>Contributors:</h4>
           <li>
-            <a href="https://www.linkedin.com/in/samantha-sexton/" target="_blank" rel="noopener noreferer">Samantha Sexton</a>
+            <a rel="noreferrer noopener" href="https://www.linkedin.com/in/samantha-sexton/" target="_blank">Samantha Sexton</a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/connietran1/" target="_blank" rel="noopener noreferer">Connie Tran</a>
+            <a rel="noreferrer noopener" href="https://www.linkedin.com/in/connietran1/" target="_blank">Connie Tran</a>
           </li>
         </ul>
       </div>
 
       <div>
         <h5>
-          This project was built in a <a href="https://sites.google.com/mintbean.io/2020-07-10-multiplayer-hackath/home?authuser=2" target="_blank" rel="noopener noreferer">multi-day hackathon</a> hosted by <a className="mintbean" href="https://www.mintbean.io/" target="_blank" rel="noopener noreferer">Mint<span>bean</span></a>
+          This project was built in a <a rel="noreferrer noopener" href="https://sites.google.com/mintbean.io/2020-07-10-multiplayer-hackath/home?authuser=2" target="_blank">multi-day hackathon</a> hosted by <a rel="noreferrer noopener" className="mintbean" href="https://www.mintbean.io/" target="_blank">Mint<span>bean</span></a>
         </h5>
       </div>
       <Link className="home-link" to="/">G o<br/><span>H o m e</span></Link>
